a5cb2b70bcea1c7fb879a959d8dd2f6767e9ef97
Dan Applegate <applegatedt@gmail.com>
2012-05-02 03:08:44 -0400

    Complete recursive solution

--- towers.py
+++ towers.py
@@ -15,15 +15,19 @@ class Towers:
 	def move(self, src, dest, spare, n):
 		"""Move n rings from the src tower to the dest tower, yielding each step"""
 		if self.__dict__[src]:
-			print n
 			if n == 1:
 				ring = self.__dict__[src].pop()
 				self.__dict__[dest].append(ring)
 				yield str(ring) + " - " + dest.upper()
 			else:
+				# Move all but biggest to spare tower
 				for s in self.move(src, spare, dest, n - 1):
 					yield s
-
+				# Move biggest to target tower
+				yield self.move(src, dest, spare, 1).next()
+				# Move all but the biggest from the spare onto destination
+				for s in self.move(spare, dest, src, n - 1):
+					yield s
 
 
 if __name__ == "__main__":
